313f5d46101a8bc3440fc8f2e56b99ba6e37e3ce
ARAVIND KRISHNAN K <84902218+ARAVIND-14-2002@users.noreply.github.com>
2023-12-15 18:38:52 +0530

    menu disabled for mobile view

--- app.js
+++ app.js
@@ -4,3 +4,37 @@ const container = document.querySelector(".container");
 hamburger_menu.addEventListener("click", () => { 
   container.classList.toggle("active");
 });
+
+        // Function to check if the device is a mobile device
+        function isMobileDevice() {
+          return window.innerWidth <= 768; // You can adjust this width as needed
+        }
+
+        // Function to show the message popup
+        function showMessagePopup() {
+          var messagePopup = document.getElementById('message-popup');
+          messagePopup.style.display = 'flex';
+        }
+
+        // Function to toggle the menu
+        function toggleMenu() {
+          var container = document.querySelector('.container');
+          container.classList.toggle('menu-active');
+        }
+
+        // Function to toggle the message popup
+        function toggleMessagePopup() {
+          var messagePopup = document.getElementById('message-popup');
+          messagePopup.style.display = (messagePopup.style.display === 'none') ? 'flex' : 'none';
+        }
+
+        // Function to close the message popup
+        function closeMessagePopup() {
+          var messagePopup = document.getElementById('message-popup');
+          messagePopup.style.display = 'none';
+        }
+
+        // Check if it's a mobile device and show the message popup
+        if (isMobileDevice()) {
+          showMessagePopup();
+        }
