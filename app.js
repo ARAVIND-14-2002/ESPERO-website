bbd60a8b35e4a0a2d0beed263a724236278d7dfe
Aravind <aravindkrishnankr@gmail.com>
2024-02-03 14:30:32 +0530

    Initial commit

--- app.js
+++ app.js
@@ -22,6 +22,48 @@ hamburger_menu.addEventListener("click", () => {
           container.classList.toggle('menu-active');
         }
 
+// Function to open the Publuu Viewer
+function openPubluuViewer() {
+  // Get the Publuu viewer container
+  var publuuViewerContainer = document.getElementById('publuu-viewer');
+
+  // Set the content of the container to the embed iframe
+  publuuViewerContainer.innerHTML = '<iframe src="https://publuu.com/flip-book/335430/771601/page/1?embed" width="100%" height="300" style="display: block; margin: 0 auto;" scrolling="no" frameborder="0" allowfullscreen="" allow="clipboard-write" class="publuuflip"></iframe>';
+
+  // Center the Publuu viewer on the screen
+  publuuViewerContainer.style.opacity = '1';
+  publuuViewerContainer.style.left = '50%';
+  publuuViewerContainer.style.transform = 'translateX(-50%)';
+
+  // For mobile view
+  var mobilePubluuViewer = document.getElementById('mobile-publuu-viewer');
+  mobilePubluuViewer.style.display = 'block';
+
+  // Request user interaction to resolve AudioContext issue
+  var resumeAudioContext = function () {
+    if (window.context) {
+      window.context.resume().then(() => {
+        console.log('AudioContext resumed');
+      });
+    }
+  };
+
+  // Create a button for user interaction
+  var interactionButton = document.createElement('button');
+  interactionButton.innerHTML = 'Start Viewer';
+  interactionButton.onclick = function () {
+    resumeAudioContext();
+    openPubluuViewer();
+    this.style.display = 'none'; // Hide the button after interaction
+  };
+
+  // Append the button to the body
+  document.body.appendChild(interactionButton);
+}
+
+
+
+
         // Function to toggle the message popup
         function toggleMessagePopup() {
           var messagePopup = document.getElementById('message-popup');
